2719a589a61beaa3d2e3e09cf669a3efbf13782e
ishiahirake <ishiahirake@gmail.com>
2019-07-09 20:59:35 +0900

    Add Binary Tree recursive traversal solutions.

---
+++ lib/TreeNode.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class TreeNode
+ */
+class TreeNode
+{
+    public $val   = null;
+    public $left  = null;
+    public $right = null;
+
+    function __construct($value)
+    {
+        $this->val = $value;
+    }
+}
