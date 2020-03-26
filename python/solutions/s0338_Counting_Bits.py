49fe1a52fad282be53c45f679dc7bf9906950404
ishiahirake <ishiahirake@gmail.com>
2020-03-26 12:50:12 +0900

    Optimize solution 338.

--- python/solutions/s0338_Counting_Bits.py
+++ python/solutions/s0338_Counting_Bits.py
@@ -9,17 +9,9 @@ class Solution:
     """
 
     def countBits(self, num: int) -> List[int]:
+        if num < 0:
+            return []
         bits = [0]
-        last_power_of_two = 0
         for i in range(1, num + 1):
-            if self.isPowerOfTwo(i):
-                bits.append(1)
-                last_power_of_two = i
-            else:
-                bits.append(1 + bits[i - last_power_of_two])
+            bits.append(bits[i & (i - 1)] + 1)
         return bits
-
-    def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
-            return False
-        return n & (n - 1) == 0
