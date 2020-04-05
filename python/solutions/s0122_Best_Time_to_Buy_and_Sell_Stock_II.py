0eacde2e0e2fb7a7bde313e3d2414055ee601904
ishiahirake <ishiahirake@gmail.com>
2020-04-05 19:37:00 +0900

    Opt solution for 122.

--- python/solutions/s0122_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ python/solutions/s0122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -11,13 +11,21 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if len(prices) < 2:
             return 0
-        to_buy = prices[0]
-        day, limit = 1, len(prices) - 1
-        while day <= limit:
-            if prices[day] < to_buy:
-                to_buy = prices[day]
-            elif to_buy < prices[day] and (day == limit or prices[day] > prices[day + 1]):
-                return prices[day] - to_buy + self.maxProfit(prices[day + 1:])
-            day += 1
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += max(0, prices[i] - prices[i - 1])
+        return profit
 
-        return 0
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     if len(prices) < 2:
+    #         return 0
+    #     to_buy = prices[0]
+    #     day, limit = 1, len(prices) - 1
+    #     while day <= limit:
+    #         if prices[day] < to_buy:
+    #             to_buy = prices[day]
+    #         elif to_buy < prices[day] and (day == limit or prices[day] > prices[day + 1]):
+    #             return prices[day] - to_buy + self.maxProfit(prices[day + 1:])
+    #         day += 1
+    #
+    #     return 0
